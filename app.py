f60f7ae51b810d209e88984cc060ef9d4216a749
healthedata1 <ehaas@healthedatainc.com>
2021-05-07 17:58:49 -0700

    update to bootstrap modal

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ as a narrative of the results
     - put cards next each other - med
     - links to to resouces -low
     - audit logging - low
+    - remove unwanted toggle columns button
 '''
 
 import dash
@@ -30,16 +31,17 @@ import numpy as np
 from dash.dependencies import Output, Input, State
 import dash_table
 import get_resources as get_res
+import dash_bootstrap_components as dbc
 
 my_path='/Users/ehaas/Documents/Python/Flask-pubsub-endpoint/' #local build
 #my_path='Flask-pubsub-endpoint/' #pythonanywhere build
 
 my_topics = {
-        "All-Topics":"all",
-        'Encounter-Start': 'http://argonautproject.org/encounters-ig/SubscriptionTopic/encounter-start',
-        'Encounter-End': 'http://argonautproject.org/encounters-ig/SubscriptionTopic/encounter-end',
-        'Encounter-Transfer': 'http://argonautproject.org/encounters-ig/SubscriptionTopic/encounter-transfer',
-         }
+ 'all': 'All-Topics',
+ 'http://argonautproject.org/encounters-ig/SubscriptionTopic/encounter-end': 'Encounter-End',
+ 'http://argonautproject.org/encounters-ig/SubscriptionTopic/encounter-start': 'Encounter-Start',
+ 'http://argonautproject.org/encounters-ig/SubscriptionTopic/encounter-transfer': 'Encounter-Transfer',
+ }
 
 my_types = [
         "all-types",
@@ -55,6 +57,8 @@ data_columns = [
         'status',
         'topic',
         'event_id',
+        'Topic',
+        'Resource ID',
         ]
 
 # data = pd.read_csv("/Users/ehaas/Documents/Python/Flask-pubsub-endpoint/data.csv")
@@ -63,15 +67,17 @@ data_columns = [
 # #new_data = data[['Date','AveragePrice']].head()
 
 
-external_stylesheets = [
-    {
-        "href": "https://fonts.googleapis.com/css2?"
-        "family=Lato:wght@400;700&display=swap",
-        "rel": "stylesheet",
-    },
-]
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# external_stylesheets = [
+#     {
+#         "href": "https://fonts.googleapis.com/css2?"
+#         "family=Lato:wght@400;700&display=swap",
+#         "rel": "stylesheet",
+#     },
+# ]
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "Da Vinci ADT Notifier!"
+
+
 
 app.layout = html.Div(
     children=[
@@ -110,7 +116,7 @@ app.layout = html.Div(
                     ),
 
                 html.P(
-                    children="Da Vinci Notifications project is planning to adopt the updated FHIR Subscriptions framework for its use cases.  This Dashboard demonstrates using subscriptions to notify a careteam member of ADT events.",
+                    children="Da Vinci Notifications project is planning to adopt the updated FHIR Subscriptions framework. This Dashboard demonstrates using subscriptions to notify a careteam member of ADT events.",
                     className="header-description",
                 ),
             ],
@@ -124,10 +130,10 @@ app.layout = html.Div(
                         dcc.Dropdown(
                             id="topic-filter",
                             options=[
-                            {"label": k, "value": v, "title": v} # change value to k when map values to name
+                            {"label": v, "value": v, "title": k} # change value to k when map values to name
                              for k,v in my_topics.items()
                             ],
-                            value="all",
+                            value="All-Topics",
                             clearable=False,
                             className="dropdown"
                         )
@@ -171,9 +177,9 @@ app.layout = html.Div(
 
                 html.Div(
                     children=dash_table.DataTable(
-                        id="noti-table",
+                        id="table",
                         columns = [ {'name' : i , 'id': i} for i in data_columns],
-                    style_cell={'textAlign': 'left'},
+                    style_cell={'textAlign': 'left','padding': '5px',},
                     # style_cell_conditional=[
                     #     {
                     #         'if': {'column_id': 'topic'},
@@ -183,20 +189,47 @@ app.layout = html.Div(
                         'backgroundColor': 'rgb(500, 230, 230)',
                         'fontWeight': 'bold'
                     },
-                    #style_as_list_view=True,
-                    # add hidden columns for topic url and event_id
+                    style_as_list_view=True,
+                    #hidden_columns=['topic','event_id',],
+                    style_data_conditional=[
+                    {'if': {'column_id': 'topic',},
+                        'display': 'None',},
+                    {'if': {'column_id': 'event_id',},
+                        'display': 'None',},
+                        ],
+                    style_header_conditional=[
+                    {'if': {'column_id': 'topic',},
+                        'display': 'None',},
+                    {'if': {'column_id': 'event_id',},
+                        'display': 'None',},
+                        ],
+
                     ),
                     className="card",
                 ),
+                # html.Div(
+                #     children=dcc.Markdown(
+                #         id = "click-data",
+                #         style={
+                #             'backgroundColor': 'rgb(255, 255, 255)',
+                #         },
+                #         ),
+                #     className="card",
+                # ),
                 html.Div(
-                    children=dcc.Markdown(
-                        id = "click-data",
-                        style={
-                            'backgroundColor': 'rgb(255, 255, 255)',
-                        },
+                    [
+                        dbc.Modal(
+                            [
+                                dbc.ModalHeader(id="header"),
+                                dbc.ModalBody(id="content"),
+                                dbc.ModalFooter(
+                                    dbc.Button("Close", id="close", className="ml-auto")
+                                ),
+                            ],
+                            id="modal",
                         ),
-                    className="card",
-                ),
+                    ]
+                )
             ],
             className="wrapper"
         )
@@ -205,36 +238,62 @@ app.layout = html.Div(
 
 # define callback
 @app.callback(
-    Output('click-data', 'children'),
-    [Input('noti-table', 'active_cell')],
+    [
+        Output("modal", "is_open"),
+        Output("header", "children"),
+        Output("content", "children"),
+        Output('table', 'active_cell'),
+    ],
+    [
+        Input('table', 'active_cell'),
+        Input("close", "n_clicks"),
+    ],
      # (A) pass table as data input to get current value from active cell "coordinates"
-    [State('noti-table', 'data')]
+    [
+        State('table', 'data'),
+        State("modal", "is_open"),
+    ]
 )
-def display_click_data(active_cell, table_data):
-    try:
+def display_click_data(active_cell, close, table_data, is_open):
+    if active_cell:
+        try:
             #cell = dumps(active_cell, indent=2)
             row = active_cell['row']
-            col = active_cell['column_id']  #use hidden column id instead when hide fullUrl
-            value = table_data[row][col]
+            #col = active_cell['column_id']  #use hidden column id instead when hide fullUrl
+            value = table_data[row]["event_id"]
             my_id = value.split('/')[-1]
             my_type = value.split('/')[-2]
             my_base = '/'.join(value.split('/')[:-2])
             #print(my_id,my_type,my_base)
+            my_header = f'### Summary of {my_type} `{my_id}`'
+            out = get_res.enc_data(
+                    my_base=my_base, #"http://hapi.fhir.org/baseR4",
+                    my_type=my_type, #"Encounter",
+                    my_id=my_id, #"2081026",
+                    )  # use value to get data through a series of Gets and return as a table of data
+        except Exception as e:
+            # log error
+            my_header = 'No resource id selected'
+            out = ''
+        return not is_open,dcc.Markdown(my_header),dcc.Markdown(out),None
+    elif close:
+        return not is_open,'','',None
+    return is_open,'','',None
 
-    except Exception as e:
-        print(e)
-        value = "no value"
-        out = 'no resource id selected'
-    else:
-        out = get_res.enc_data(
-                my_base=my_base, #"http://hapi.fhir.org/baseR4",
-                my_type=my_type, #"Encounter",
-                my_id=my_id, #"2081026",
-                )  # use value to get data through a series of Gets and return as a table of data
-        out = f'### Summary of {my_type} {my_id}\n\n{out}'
-
-    #return f'{value} gets you: {dumps(out, indent=2)}'
-    return out
+    # except Exception as e:
+    #     print(e)
+    #     value = "no value"
+    #     out = 'no resource id selected'
+    # else:
+    #     out = get_res.enc_data(
+    #             my_base=my_base, #"http://hapi.fhir.org/baseR4",
+    #             my_type=my_type, #"Encounter",
+    #             my_id=my_id, #"2081026",
+    #             )  # use value to get data through a series of Gets and return as a table of data
+    #     out = f'### Summary of {my_type} {my_id}\n\n{out}'
+    #
+    # #return f'{value} gets you: {dumps(out, indent=2)}'
+    # return out
 
 @app.callback(
     [
@@ -264,7 +323,7 @@ def update_period(n_intervals, start_date, end_date):
 @app.callback(
     [
         #Output("price-chart", "figure"),
-        Output("noti-table", "data"),
+        Output("table", "data"),
     ],
     [
         Input("topic-filter", "value"),
@@ -285,14 +344,17 @@ def update_charts(topic, my_type, start_date, end_date, n_intervals):
         )
     data = data.loc[mask, :]
 
-    if  topic != 'all':
-        data = data.loc[data["topic"]==topic] #sort by name
+    if  topic != 'All-Topics':
+        my_topic = [k for k,v in my_topics.items() if v == topic] # map back to url
+        data = data.loc[data["topic"]==my_topic[0]]
     if  my_type != 'all-types':
         data = data.loc[data["type"]==my_type]
 
     sorted_filtered_data = data.sort_values("timestamp", ascending=False)
-    #map to topic names
-    #add id only column
+    Topic = [my_topics[i] for i in sorted_filtered_data['topic']]#map to topic names
+    sorted_filtered_data['Topic'] = Topic
+    Resource_ID = [str(i).split('/')[-1] for i in sorted_filtered_data['event_id']]#add id only column
+    sorted_filtered_data['Resource ID'] = Resource_ID
     volume_chart_data = sorted_filtered_data.to_dict('records')
 
     return (
